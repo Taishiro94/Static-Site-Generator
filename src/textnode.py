ec7443e252ff36ae05de30036a912e8bfb5d4031
Taishiro94 <tobias.krause.privat@gmail.com>
2026-05-02 21:32:37 +0200

    CH2: Nodes - L6:TextNode to HTMLNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -19,3 +20,29 @@ class TextNode():
     
     def __repr__(self):
        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    value = text_node.text
+    props = None
+    match(text_node.text_type):
+        case TextType.TEXT:
+            tag = None
+        case TextType.BOLD:
+            tag = "b"
+        case TextType.ITALIC:
+            tag = "i"
+        case TextType.CODE:
+            tag = "code"
+        case TextType.LINK:
+            tag = "a"
+            props = {"href": text_node.url,}
+        case TextType.IMAGE:
+            tag = "img"
+            value = ""
+            props = {"src": text_node.url, "alt": text_node.text}
+
+        case _:
+            raise ValueError(f"invalid text type: {text_node.text_type}")
+    
+
+    return LeafNode(tag, value, props)
