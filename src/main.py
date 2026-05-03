0d5711c347d3a5c19592b3eb6a18dac7ce864b37
Taishiro94 <tobias.krause.privat@gmail.com>
2026-05-03 17:45:21 +0200

    CH4: Blocks - L3: Block to HTML

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from textnode import TextNode, TextType
-from md_converter import text_to_textnodes
+from md_converter import markdown_to_html_node
 
 def main():
-    Node = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+    md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+    Node = markdown_to_html_node(md)
     print(Node)
 
 main()
