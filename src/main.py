118fb615a92805852bcd16a2e2548a7e88a3e519
Taishiro94 <tobias.krause.privat@gmail.com>
2026-05-04 00:17:16 +0200

    CH5: Website - L1: Copy Static | Example Solution

--- src/main.py
+++ src/main.py
@@ -1,62 +1,19 @@
-import os, shutil
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    inhalt= get_files("static/")
-    move_files(inhalt)
-    
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-def get_files(pfad):
-    results=[]
-    inhalt = os.listdir(pfad)
-    for element in inhalt: 
-        if len(element.split(".")) == 2:
-            results.append(pfad + element)
-        else:
-            x = get_files(pfad + element+"/")
-            for i in x:
-                results.append(i)
-    return results
-
-def move_files(pfadlist):
-    workpaths = []
-    for pfad in pfadlist:
-        pfady=""
-        workpfad = pfad.split("/", 1)
-        workpfad[0] = "public/"
-        for p in workpfad:
-            pfady+=p
-        workpaths.append(pfady)
-    if os.path.exists("public/"):
-        shutil.rmtree("public/")
-    os.mkdir("public/")
-
-    #Create paths    
-    for pfad in workpaths:
-        teilpfad = pfad.split("/")
-        while len(teilpfad) > 2:
-            teil_1 = teilpfad[0]
-            teil_2 = "/" + teilpfad[1] + "/"
-            os.mkdir(teil_1 + teil_2)
-            new_teilpfad = [teil_1 + teil_2]
-            for part in teilpfad:
-                if part == teilpfad[0] or part == teilpfad[1]:
-                    continue
-                else:
-                    new_teilpfad.append(part)
-            teilpfad = new_teilpfad
-    i=0
-    for pfad in pfadlist:
-        shutil.copy(pfad, workpaths[i])
-        i+=1
-
-        
-
-        
-        
-            
-        
-
-def copy_static():
-    pass
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 main()
